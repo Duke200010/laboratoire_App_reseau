e8b6e9fa09983d55df3f9fd03fe303dd09219a64
MAYATTA NDIAYE <mndiaye27@etu.uqac.ca>
2025-01-15 18:15:27 -0500

    Laboratoire 1 d'application reseau et securite informatique

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+import html
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def accueil():
+    if request.method == "POST":
+        # Validation des entrées pour éviter les injections
+        nom = html.escape(request.form.get("nom"))
+        ville = html.escape(request.form.get("ville"))
+        return render_template("accueil.html", nom=nom, ville=ville, formulaire=False)
+    
+    return render_template("accueil.html", formulaire=True)
+
+@app.route("/static/<path:filename>")
+def static_file(filename):
+    return app.send_static_file(filename)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("erreur.html"), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
